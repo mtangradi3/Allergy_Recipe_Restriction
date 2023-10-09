5f401f19c897b1a0c5027047e35618dcf8cd91e6
spham00 <sarahpham075@gmail.com>
2023-10-09 17:01:54 -0400

    Added button with functioning dropdown.

--- client/src/components/UserProfile-Buttons/Groups.js
+++ client/src/components/UserProfile-Buttons/Groups.js
@@ -1,18 +1,37 @@
 // Example for Allergies.js
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/Buttons.css"
 
+function DropdownContent() {
+    return (
+        <div className="dropdown-content">
+            {/* Add your content for the dropdown here */}
+            <p>Information 1</p>
+            <p>Information 2</p>
+            <p>Information 3</p>
+        </div>
+    );
+}
+
 function Groups() {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
   return (
       <div>
         <section>
           <h2>My Groups</h2>
-            <button
-                className="buttonStyle"
-                //onClick={() => navigate("/user-profile/allergies")}
-            >
-                Test Group
-            </button>
+            <div className={`dropdown ${isDropdownOpen ? 'active' : ''}`} onClick={toggleDropdown}>
+                <button className="buttonStyle">
+                    {isDropdownOpen ? 'Hide Content' : 'Show Content'}
+                </button>
+                <div className="dropdown-content-container">
+                    {isDropdownOpen && <DropdownContent />}
+                </div>
+            </div>
         </section>
 
         <section>
