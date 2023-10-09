1b4616d8326bfb5a7b04abbf4039b06cf90ca8af
spham00 <sarahpham075@gmail.com>
2023-10-09 18:45:11 -0400

    Fixed issue where clicking content would hide it.

--- client/src/components/UserProfile-Buttons/Groups.js
+++ client/src/components/UserProfile-Buttons/Groups.js
@@ -1,5 +1,5 @@
 // Example for Allergies.js
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "../../styles/Buttons.css"
 
 function DropdownContent() {
@@ -15,17 +15,34 @@ function DropdownContent() {
 
 function Groups() {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const dropdownRef = useRef(null);
 
     const toggleDropdown = () => {
         setIsDropdownOpen(!isDropdownOpen);
     };
 
+    const handleClickOutside = (event) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+            setIsDropdownOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        // Add click event listener to close dropdown when clicking outside
+        document.addEventListener("click", handleClickOutside);
+
+        return () => {
+            // Remove the click event listener on component unmount
+            document.removeEventListener("click", handleClickOutside);
+        };
+    }, []);
+
   return (
       <div>
         <section>
           <h2>My Groups</h2>
-            <div className={`dropdown ${isDropdownOpen ? 'active' : ''}`} onClick={toggleDropdown}>
-                <button className="buttonStyle">
+            <div className={`dropdown ${isDropdownOpen ? 'active' : ''}`} ref={dropdownRef}>
+                <button className="buttonStyle" onClick={toggleDropdown}>
                     {isDropdownOpen ? 'Hide Content' : 'Show Content'}
                 </button>
                 <div className="dropdown-content-container">
