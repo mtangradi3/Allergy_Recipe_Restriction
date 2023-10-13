3e3afce6a5300592bb7584acf4e9ad92f99ee846
spham00 <sarahpham075@gmail.com>
2023-10-13 14:32:02 -0400

    Added pop-up to come up when the plus sign button is clicked.

--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -5,6 +5,11 @@
 
 export const API_TEST = "http://localhost:8080/api/controllers/testController";
 
+// For user login, creation
 export const INSERT_NEW_USER = "http://localhost:8080/api/user/create_user";
 
 export const GET_ALL_USERS = "http://localhost:8080/api/user/get_all_users";
+
+// For group creation, displaying all groups
+// export const GET_ALL_GROUPS = "http://localhost:8080/api/user/get_all_users"
+export const INSERT_NEW_GROUP = "http://localhost:8080/api/group/create_new_group"
