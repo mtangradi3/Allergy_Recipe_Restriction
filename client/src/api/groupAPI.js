b53c8bf7f1dddfd0c48822e2d469ffbfec2c70b9
spham00 <sarahpham075@gmail.com>
2023-10-17 15:25:01 -0400

    Added necessary functions to connect to api calls.

--- client/src/api/groupAPI.js
+++ client/src/api/groupAPI.js
@@ -5,23 +5,23 @@
  */
 
 import axios from "axios";
-import {GET_ALL_USERS, INSERT_NEW_GROUP, INSERT_NEW_USER} from "../utils/constant";
+import { GET_ALL_GROUPS, INSERT_NEW_GROUP, GET_USERS_IN_GROUP, ADD_USER_TO_GROUP } from "../utils/constant";
 
 /**
  * this function will get all the groups to display
  */
-// export const getAllGroups = async () => {
-//     try {
-//         const response = await axios.get(GET_ALL_USERS, {
-//             headers: {
-//                 "Content-Type": "application/x-www-form-urlencoded",
-//             },
-//         });
-//         return response.data;
-//     } catch (error) {
-//         throw error;
-//     }
-// };
+export const getAllGroups = async () => {
+    try {
+        const response = await axios.get(GET_ALL_GROUPS, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        });
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
 
 /**
  * this function will create a new group
@@ -35,6 +35,50 @@ export const createGroup = async (groupName) => {
 
     try {
         const response = await axios.post(INSERT_NEW_GROUP, params, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        });
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+/**
+ * this function will get the users in the group (for display purposes)
+ *
+ * @param groupName
+ * @returns {Promise<any>} an error if there is a duplicate
+ */
+export const getUsersInGroup = async (groupName) => {
+    const params = new URLSearchParams();
+    params.append("group_name", groupName);
+    try {
+        const response = await axios.post(GET_USERS_IN_GROUP, params, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        });
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+/**
+ * this function will add the current logged in user to the group
+ *
+ * @param email
+ * @param groupName
+ * @returns {Promise<any>} an error if there is a duplicate
+ */
+export const addUserToGroup = async (email, groupName) => {
+    const params = new URLSearchParams();
+    params.append("email", email);
+    params.append("group_name", groupName);
+    try {
+        const response = await axios.post(ADD_USER_TO_GROUP, params, {
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
             },
--- client/src/utils/constant.js
+++ client/src/utils/constant.js
@@ -11,5 +11,7 @@ export const INSERT_NEW_USER = "http://localhost:8080/api/user/create_user";
 export const GET_ALL_USERS = "http://localhost:8080/api/user/get_all_users";
 
 // For group creation, displaying all groups
-// export const GET_ALL_GROUPS = "http://localhost:8080/api/user/get_all_users"
-export const INSERT_NEW_GROUP = "http://localhost:8080/api/group/create_new_group"
+export const GET_ALL_GROUPS = "http://localhost:8080/api/group/get_all_groups_name";
+export const INSERT_NEW_GROUP = "http://localhost:8080/api/group/create_new_group";
+export const GET_USERS_IN_GROUP = "http://localhost:8080/api/group/get_users_in_group";
+export const ADD_USER_TO_GROUP = "http://localhost:8080/api/user/add_user_to_group";
