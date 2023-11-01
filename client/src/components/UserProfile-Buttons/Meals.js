9044b6a5d8faabb5d15f1f9676bf1f5c2585c1f2
Phamandy2000 <114016939+Phamandy2000@users.noreply.github.com>
2023-10-31 20:18:56 -0400

    Meals page now has drop down, and create meal button

--- client/src/components/UserProfile-Buttons/Meals.js
+++ client/src/components/UserProfile-Buttons/Meals.js
@@ -1,18 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { getAllMealsWithAllergy } from "../../api/mealAPI";
+import { getAllMealsWithAllergy, getMealIngredients } from "../../api/mealAPI";
 import "../../App.css";
-import { useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function Meals() {
   const [meals, setMeals] = useState([]);
   const [error, setError] = useState(null);
+  const [expandedMealIndex, setExpandedMealIndex] = useState(null);
+  const navigate = useNavigate();
   const location = useLocation();
-  const { email } = location.state || {}; // Get the email from the state
+  const { email } = location.state || {};
 
   useEffect(() => {
     const fetchSuitableMeals = async () => {
       try {
-        const data = await getAllMealsWithAllergy(email); // Fetch meals based on the user's email
+        const data = await getAllMealsWithAllergy(email);
         setMeals(data);
       } catch (err) {
         setError(err.message || "An error occurred while fetching meals.");
@@ -22,23 +24,40 @@ function Meals() {
     fetchSuitableMeals();
   }, [email]);
 
+  const handleMealClick = async (mealName, index) => {
+    const newIndex = index === expandedMealIndex ? null : index;
+    setExpandedMealIndex(newIndex);
+
+    if (newIndex !== null) {
+      try {
+        const ingredients = await getMealIngredients(mealName);
+        const updatedMeals = [...meals];
+        updatedMeals[index].ingredients = ingredients;
+        setMeals(updatedMeals);
+      } catch (error) {
+        console.error("Failed to fetch ingredients:", error);
+        setError("Failed to fetch ingredients.");
+      }
+    }
+  };
+
   return (
     <div className="meal-container">
       <h1>Meals Suitable for Your Allergies</h1>
-
       {error && <div>Error: {error}</div>}
-
       <ul>
         {meals.map((meal, index) => (
-          <li key={index}>
-            <h2>{meal.meal_name}</h2>
+          <li key={meal.meal_name}>
+            <h2 onClick={() => handleMealClick(meal.meal_name, index)}>
+              {meal.meal_name}
+            </h2>
             {meal.meal_image && (
               <img
                 src={`data:image/jpeg;base64,${meal.meal_image}`}
                 alt={meal.meal_name}
               />
             )}
-            {meal.ingredients && (
+            {index === expandedMealIndex && meal.ingredients && (
               <>
                 <p>Ingredients:</p>
                 <ul>
@@ -51,6 +70,9 @@ function Meals() {
           </li>
         ))}
       </ul>
+      <button onClick={() => navigate("/create-meal", { state: { email } })}>
+        Create New Meal
+      </button>
     </div>
   );
 }
