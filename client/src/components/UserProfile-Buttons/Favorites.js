c945e0de356c2e844e49fdc406ec606e38835244
spham00 <sarahpham075@gmail.com>
2023-11-17 02:42:04 -0500

    Added functionality where clicking on a favorite leads to that meal's page.

--- client/src/components/UserProfile-Buttons/Favorites.js
+++ client/src/components/UserProfile-Buttons/Favorites.js
@@ -1,23 +1,21 @@
 import React, { useState, useEffect } from "react";
 import "../../styles/Buttons.css";
 import { getUserFavoritesMeal } from "../../api/userAPI";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function Favorites() {
   const [favorites, setFavorites] = useState([]);
   const location = useLocation();
+  const navigate = useNavigate();
   const { email } = location.state || {};
 
   async function fetchUserFavoritesMeal(email) {
     try {
-      const data = await getUserFavoritesMeal(email); // Await the Promise
-
-      // Assuming data is an array, you can map over it
+      const data = await getUserFavoritesMeal(email);
       const favoritesWithIds = data.map((favorite, index) => ({
         id: index + 1,
         name: favorite,
       }));
-
       return favoritesWithIds;
     } catch (error) {
       console.error("Error fetching favorites:", error);
@@ -35,13 +33,21 @@ function Favorites() {
       });
   }, [email]);
 
+  const handleFavoriteClick = (favorite) => {
+    navigate(`/meal-details/${favorite.name}`, {
+      state: { email, meal: favorite, favorites },
+    });
+  };
+
   return (
     <div>
       <h1>Favorites Page</h1>
       <ul>
         {favorites.length > 0 ? (
           favorites.map((favorite) => (
-            <li key={favorite.id}>{favorite.name}</li>
+            <li key={favorite.id} onClick={() => handleFavoriteClick(favorite)}>
+              {favorite.name}
+            </li>
           ))
         ) : (
           <li>No favorites found</li>
