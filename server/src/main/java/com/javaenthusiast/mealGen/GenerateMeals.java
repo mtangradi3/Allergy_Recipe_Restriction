9f65b985eb12aee3d923bc7a3863d5dfc47759bd
Marcus Tangradi <mtangradi5@gmail.com>
2023-11-27 10:35:21 -0500

    created static method for getting meals from ingredients

---
+++ server/src/main/java/com/javaenthusiast/mealGen/GenerateMeals.java
@@ -0,0 +1,15 @@
+package com.javaenthusiast.mealGen;
+
+import java.util.ArrayList;
+
+/**
+ * Author: Marcus Tangradi
+ */
+public class GenerateMeals {
+
+
+    public static void  getMealsFromIngredients(ArrayList<String> ingredients){
+
+
+    }
+}
