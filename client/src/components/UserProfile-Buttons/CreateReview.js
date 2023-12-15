2dde99788e2b6d90a780d5d958b496c64d3fc98d
hopkin53 <hopkin53@students.rowan.edu>
2023-12-14 19:37:22 -0500

    added image to review page

--- client/src/components/UserProfile-Buttons/CreateReview.js
+++ client/src/components/UserProfile-Buttons/CreateReview.js
@@ -4,14 +4,31 @@ import Form from "react-bootstrap/Form";
 import 'bootstrap/dist/css/bootstrap.css';
 import {useLocation} from "react-router-dom";
 import Button from "react-bootstrap/Button";
-import {newRating, removeUserReview} from "../../api/userAPI";
-import {getMealReviews} from "../../api/mealAPI";
+import {getUserFavoritesMeal, newRating, removeUserReview} from "../../api/userAPI";
+import {getAllMeals, getMealReviews} from "../../api/mealAPI";
 import Stack from "react-bootstrap/Stack";
-
 
 
 function CreateReview() {
 
+
+  async function fetchImage() {
+    try {
+      const allMealWImages = await getAllMeals(email);
+      const images = allMealWImages.map((meal) => ({
+        mealImage: meal.meal_image,
+        mealName: meal.meal_name,
+      }));
+
+      const imageObj = images.find(
+          (image) => image.mealName === meal_name
+      );
+      setMealImage(imageObj.mealImage);
+    } catch (error) {
+      console.error("Error fetching images:", error);
+      return [];
+    }
+  }
 
 
   useEffect(() => {
@@ -24,11 +41,13 @@ function CreateReview() {
         console.log(err.message || "An error occurred while fetching Allergies.");
       }
     };
+    fetchImage()
 
     fetchRatings()
 
 
   }, []);
+
 
 
   const location = useLocation();
@@ -38,6 +57,7 @@ function CreateReview() {
   const [userRating, setUserRating] = useState(null);
   const [UserReview, setUserReview] = useState('');
   const [ratingList, setRatingList] = useState([]);
+  const [mealImage, setMealImage] = useState();
 
   const handleRemoveReview = (index) => {
     try {
@@ -82,17 +102,23 @@ function CreateReview() {
   };
 
   return <div>
-    <h1>Reviews for {meal_name}</h1>
+    <h1>Reviews for {meal_name}
+    <img
+        src={`data:image/jpeg;base64, ${mealImage ? mealImage : " "}`}
+        alt={' '}
+        width="100"
+        height="100"
+    /></h1>
     <Form>
       <Form.Label><h4>New Rating</h4></Form.Label>
       <Form.Group className="mb-3" controlId="rating">
         <Form.Label>Rating</Form.Label>
-        <Form.Control placeholder="0-5" onChange={(e) => setUserRating(parseInt(e.target.value))} />
+        <Form.Control placeholder="0-5" onChange={(e) => setUserRating(parseInt(e.target.value))}/>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="review">
         <Form.Label>Review (optional)</Form.Label>
-        <Form.Control as="textarea"   rows={3} onChange={(e) => setUserReview(e.target.value)} />
+        <Form.Control as="textarea" rows={3} onChange={(e) => setUserReview(e.target.value)}/>
       </Form.Group>
       <Button variant="primary" onClick={handleFormSubmit}>
         Submit
@@ -108,28 +134,29 @@ function CreateReview() {
             gap={3}
             className="mb-2 p-2 border rounded"
 
-        ><div>
-          <div className="p-2">Name: {item.name}</div>
-          <div className="p-2">Rating: {item.rating}</div>
-          <div className="p-2">
-            {(item.review )  && (
-                <div>
-                  Review: {item.review}
-                </div>
-            )}
+        >
+          <div>
+            <div className="p-2">Name: {item.name}</div>
+            <div className="p-2">Rating: {item.rating}</div>
+            <div className="p-2">
+              {(item.review) && (
+                  <div>
+                    Review: {item.review}
+                  </div>
+              )}
+            </div>
+
+
           </div>
-
-
-        </div>
           <div className="p-2 ms-auto">
-            {(item.name === firstName + ' ' + lastName)  && (
-            <Button
-              variant="outline-danger"
-              size="sm"
-              onClick={() => handleRemoveReview(index)}
-          >
-            Remove
-          </Button>
+            {(item.name === firstName + ' ' + lastName) && (
+                <Button
+                    variant="outline-danger"
+                    size="sm"
+                    onClick={() => handleRemoveReview(index)}
+                >
+                  Remove
+                </Button>
             )}
           </div>
 
